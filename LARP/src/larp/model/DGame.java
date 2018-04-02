b577c3e1ee0260153ad0f0e973ae32eec1aa46d0
Timothy Lloyd <timothyalba.official@gmail.com>
2018-04-02 16:08:31 -0500

    Updated DGame with methods to allow access to current room, player's Knight, and change room.

--- LARP/src/larp/model/DGame.java
+++ LARP/src/larp/model/DGame.java
@@ -6,6 +6,8 @@
 package larp.model;
 
 import larp.model.inventory.Item;
+import larp.model.room.object.*;
+import larp.model.character.*;
 import java.util.ArrayList;
 /**
  *
@@ -13,26 +15,80 @@ import java.util.ArrayList;
  */
 public class DGame {
     
-    private final ArrayList<Item> LOOT;
     private static DGame instance = null;
+    private final ArrayList<Item> LOOT;
+    private ArrayList<DRoom> rooms;
+    private Knight player;
+    private DRoom currentRoom;
     
     protected DGame(){
         LOOT = new ArrayList<>();
     }
     
+    /**
+     * Get the set static instance for DGame, may be null if not set.
+     * @return Returns the instance of DGame or null if no instance is set.
+     */
     public static DGame getInstance(){
-        if(instance == null)
-            instance = new DGame();
         return instance;
+    }
+    
+    /**
+     * Used to set the static instance of DGame so other classes can reference it.
+     * @param game The game object to set the static instance to.
+     */
+    public static void setInstance(DGame game){
+        instance = game;
     }
     
     private ArrayList instanceLoot(){
         return LOOT;
     }
     
+    /**
+     * Get the entire list of available game loot.
+     * @return Returns a java.util.ArrayList listing the available game loot, or
+     * returns null if no instance of DGame is set.
+     */
     public static ArrayList getLoot(){
         if(instance == null)
             return null;
         return instance.instanceLoot();
+    }
+    
+    /**
+     * Get the current room the player is in.
+     * @return Returns the room the player is currently in.
+     */
+    public DRoom getCurrentRoom(){
+        return currentRoom;
+    }
+    
+    /**
+     * This method returns the player's Knight object.
+     * @return Returns the Knight object representing the player in game.
+     */
+    public Knight getPlayer(){
+        return player;
+    }
+    
+    /**
+     * Pass the DGame object a door and it will look for and return the next room,
+     * setting the current room to the new room. If the door passed is null or
+     * the room the door points to doesn't exist the current room the player is
+     * in will be returned.
+     * @param door The door in the current room that points to the next room.
+     * @return Returns either the next room and sets current room to it OR returns
+     * the current room the player is in if there is an issue with the door.
+     */
+    public DRoom changeRoom(Door door){
+        if(door != null){
+            int temp = door.getNextRoom();
+            if(temp < rooms.size() && temp >= 0){
+                currentRoom = rooms.get(temp);
+                return currentRoom;
+            }
+        }
+        return currentRoom;
     }
 }
