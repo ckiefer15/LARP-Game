c2a0836a76a52c6ce42e2f7831d833ff4929c390
Timothy Lloyd <timothyalba.official@gmail.com>
2018-04-24 00:21:25 -0500

    Added Larp Release page

---
+++ LarpRelease/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>LARP Latest Release</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div>
+		<h1>LARP Latest Release and Guide</h1>
+	</div>
+	<hr>
+	<div>
+		<h2>Gameplay Guide</h2>
+		<table>
+			<tr>
+				<th>Movement</th>
+				<td>Use WASD or the arrow keys to move the character around.</td>
+			</tr>
+			<tr>
+				<th>Access Inventory</th>
+				<td>On the dungeon screen press 'i' to enter inventory. To exit press 'esc' or the green arrow in the top left of the inventory.</td>
+			</tr>
+			<tr>
+				<th>Return to Main Menu</th>
+				<td>From the dungeon screen press 'm' to return to main menu. WARNING! Your game will be reset if you return to main menu!</td>
+			</tr>
+			<tr>
+				<th>Game Objective</th>
+				<td>Explore the dungeon, collect chests, and find and defeat the boss! (He's at the end of the lava room ;D)</td>
+			</tr>
+		</table>
+	</div>
+	<hr>
+	<div>
+		<a href="Larp_Release.zip"><h1>Download the Latest LARP Release</h1></a>
+	</div>
+</body>
+</body>
+</html>
